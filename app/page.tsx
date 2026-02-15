e3318410d8eb47621f40925a2f5505e151a296eb
Daniel Mamani <danymamani1002@gmail.com>
2026-02-15 18:36:33 -0300

    feat: integrate FeaturedProjects section in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "../components/sections/Hero";
+import FeaturedProjects from '../components/sections/FeaturedProjects';
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white px-6 md:px-24 font-sans selection:bg-white selection:text-black">
       <div className="max-w-5xl mx-auto">
         <Hero />
+        <FeaturedProjects />
       </div>
     </main>
   );
